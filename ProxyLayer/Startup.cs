e4958d07fb2f16261438897f72225da39df0fa69
Xavier Varricatt <xaviervarricatt@live.in>
2019-02-07 11:52:17 +0530

    Added Default Configuration to DI.

--- ProxyLayer/Startup.cs
+++ ProxyLayer/Startup.cs
@@ -32,6 +32,8 @@ namespace ProxyLayer
             {
                 options.OutputFormatters.Add(new OctetOutputFormatter());
             }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
