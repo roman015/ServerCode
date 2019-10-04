e3492b65216e6c4b154a9d1a940566de28656f14
Xavier <xaviervarricatt@live.in>
2019-10-04 21:58:21 +0530

    DefaultController with Robots.txt added

---
+++ ProxyLayer/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProxyLayer.Controllers
+{
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("robots.txt")]
+        public IActionResult GetRobotsSettings()
+        {
+            // TODO : Read this from a text file
+            return Ok("User-agent: *\r\nDisallow: /");
+        }        
+    }
+}
